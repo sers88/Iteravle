668ed9bd624844de8e71a0cef8445d2229ab7858
ksantd <admin@ksantd.com>
2023-02-05 00:24:48 +0300

    init commit

---
+++ src/main/java/org/example/Randoms.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            private final Random random = new Random();
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+            }
+        };
+    }
+}
